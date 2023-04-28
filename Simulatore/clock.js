676c61cdbda481774fccfa06b1fe7c481277f552
Leonardo Campagna <leonardo.campagna@stud.itsaltoadriatico.it>
2023-04-28 17:17:25 +0200

    bozza bpm

--- Simulatore/clock.js
+++ Simulatore/clock.js
@@ -6,6 +6,20 @@ let position = {
     latitude: (Math.random() * 180) - 90,
     longitude: (Math.random() * 360) -180}
 
+function generateStartingBpm(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getBpm() {
+    let startingBpm = generateStartingBpm(80, 120)
+    let chance = Math.random();
+    if (chance < 0.5) startingBpm+10
+    else startingBpm-10
+    bpm = startingBpm
+}
+
 const sim = () => {
     let ran = Math.random()
     if(ran < 0.25){
@@ -24,6 +38,7 @@ const sim = () => {
         position.latitude -= Math.random()
         position.longitude -= Math.random()
     }
+    getBpm()
     let res = {
         sessionUUID,
         pools,
