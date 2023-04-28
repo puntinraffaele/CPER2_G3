0384fb03b49c5f5218e62d489a5f879df11c4551
Raffaele Puntin <raffaele,puntin@stud.itsaltoadriatico.it>
2023-04-28 17:37:07 +0200

    Add high and low longitude managing

--- Simulatore/clock.js
+++ Simulatore/clock.js
@@ -7,22 +7,44 @@ let position = {
     longitude: (Math.random() * 360) -180}
 
 const sim = () => {
-    let ran = Math.random()
-    if(ran < 0.25){
+    let random = Math.random()
+    if(random < 0.15){
         position.latitude += Math.random()
         position.longitude += Math.random()
     }
-    if(ran >= 0.25 && ran < 0.5){
+    if(random >= 0.15 && random < 0.3){
         position.latitude += Math.random()
         position.longitude -= Math.random()
     }
-    if(ran >= 0.5 && ran < 0.75){
+    if(random >= 0.3 && random < 0.45){
         position.latitude -= Math.random()
         position.longitude += Math.random()
     }
-    if(ran > 0.75){
+    if(random >= 0.45 && random < 0.6){
         position.latitude -= Math.random()
+    }
+    if(random >= 0.6 && random < 0.75){
+        position.latitude += Math.random()
+    }
+    if(random >= 0.75 && random < 0.9){
         position.longitude -= Math.random()
+    }
+    if(random >= 0.9){
+        position.longitude += Math.random()
+    }
+    if(position.latitude > 90){
+        
+    }
+    if(position.latitude < -90){
+
+    }
+    if(position.longitude > 180){
+        let delta = position.longitude - 180
+        position.longitude = -180 + delta
+    }
+    if(position.longitude <= -180){
+        let delta = position.longitude + 180
+        position.longitude = 180 - delta
     }
     let res = {
         sessionUUID,
