3d258f8361c4e9ac588478d45a2953acfd4762e9
Raffaele Puntin <raffaele,puntin@stud.itsaltoadriatico.it>
2023-04-28 17:06:40 +0200

    Started position managing

--- Simulatore/clock.js
+++ Simulatore/clock.js
@@ -2,9 +2,28 @@
 const sessionUUID = require('uuid').v4()
 let pools = 0
 let bpm = 0
-let position = [0, 0]
+let position = {
+    latitude: (Math.random() * 180) - 90,
+    longitude: (Math.random() * 360) -180}
 
 const sim = () => {
+    let ran = Math.random()
+    if(ran < 0.25){
+        position.latitude += Math.random()
+        position.longitude += Math.random()
+    }
+    if(ran >= 0.25 && ran < 0.5){
+        position.latitude += Math.random()
+        position.longitude -= Math.random()
+    }
+    if(ran >= 0.5 && ran < 0.75){
+        position.latitude -= Math.random()
+        position.longitude += Math.random()
+    }
+    if(ran > 0.75){
+        position.latitude -= Math.random()
+        position.longitude -= Math.random()
+    }
     let res = {
         sessionUUID,
         pools,
