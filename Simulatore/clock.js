1c76f2518ce4f3da36a5b38679161aaa9e3a28e7
Federico Mattiuzzi <79696658+JLarn@users.noreply.github.com>
2023-05-03 09:56:07 +0200

    configurazione in file .env

--- Simulatore/clock.js
+++ Simulatore/clock.js
@@ -1,66 +1,75 @@
 // Prima bozza molto brutta di un simulatore di smartwatch
-const sessionUUID = require('uuid').v4()
-let pools = 0
-let bpm = 0
+require('dotenv').config()
+
+const sessionUUID = require('uuid').v4();
+let pools = 0;
+let bpm = 0;
 let position = {
     latitude: (Math.random() * 180) - 90,
-    longitude: (Math.random() * 360) -180}
+    longitude: (Math.random() * 360) - 180
+};
 
 function generateStartingBpm(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
+};
 
 function getBpm() {
-    let startingBpm = generateStartingBpm(80, 120)
+    let startingBpm = generateStartingBpm(80, 120);
     let chance = Math.random();
-    if (chance < 0.5) startingBpm+10
-    else startingBpm-10
-    bpm = startingBpm
-}
+    if (chance < 0.5) startingBpm + 10;
+    else startingBpm - 10;
+    bpm = startingBpm;
+};
 
 const sim = () => {
-    let random = Math.random()
-    if(random < 0.15){
-        position.latitude += Math.random()
-        position.longitude += Math.random()
+    let random = Math.random();
+    if (random < 0.15) {
+        position.latitude += Math.random();
+        position.longitude += Math.random();
     }
-    if(random >= 0.15 && random < 0.3){
+    else if (random >= 0.15 && random < 0.3) {
+        position.latitude += Math.random();
+        position.longitude -= Math.random();
+    }
+    else if (random >= 0.3 && random < 0.45) {
+        position.latitude -= Math.random();
+        position.longitude += Math.random();
+    }
+    else if (random >= 0.45 && random < 0.6) {
+        position.latitude -= Math.random()
+    }
+    else if (random >= 0.6 && random < 0.75) {
         position.latitude += Math.random()
+    }
+    else if (random >= 0.75 && random < 0.9) {
         position.longitude -= Math.random()
     }
-    if(random >= 0.3 && random < 0.45){
-        position.latitude -= Math.random()
+    else if (random >= 0.9) {
         position.longitude += Math.random()
     }
-    if(random >= 0.45 && random < 0.6){
-        position.latitude -= Math.random()
-    }
-    if(random >= 0.6 && random < 0.75){
-        position.latitude += Math.random()
-    }
-    if(random >= 0.75 && random < 0.9){
-        position.longitude -= Math.random()
-    }
-    if(random >= 0.9){
-        position.longitude += Math.random()
-    }
-    if(position.latitude > 90){
-        
-    }
-    if(position.latitude < -90){
+
+
+
+    if (position.latitude > 90) {
 
     }
-    if(position.longitude > 180){
+    else if (position.latitude < -90) {
+
+    }
+
+    if (position.longitude > 180) {
         let delta = position.longitude - 180
         position.longitude = -180 + delta
     }
-    if(position.longitude <= -180){
+    else if (position.longitude <= -180) {
         let delta = position.longitude + 180
         position.longitude = 180 - delta
     }
+
     getBpm()
+
     let res = {
         sessionUUID,
         pools,
@@ -71,4 +80,4 @@ const sim = () => {
     console.log(res)
 }
 sim()
-setInterval(sim, 10000)
+setInterval(sim, process.env.INTERVAL * 1000)
