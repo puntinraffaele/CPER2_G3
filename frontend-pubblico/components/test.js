a140439df8b7e00df934cc0fc641f9d2e323cc0b
Federico Mattiuzzi <79696658+JLarn@users.noreply.github.com>
2023-06-01 12:18:53 +0200

    sistemata fetch (?)
    ~funge ma speriamo bene

--- frontend-pubblico/components/test.js
+++ frontend-pubblico/components/test.js
@@ -1,22 +1,24 @@
 import { useState, useEffect } from 'react';
- 
-export default function FetchTest() {
+const baseURL = 'https://cper2g3earth4sportazurefunction.azurewebsites.net/api/get_device_data?uuid=69d66243-e51c-474d-a41b-fd4ed1a91a42'
+
+export default function DeviceData() {
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(false);
- 
+
   useEffect(() => {
     setLoading(true);
-    fetch('https://cper2g3earth4sportazurefunction.azurewebsites.net/api/get_device_data?uuid=69d66243-e51c-474d-a41b-fd4ed1a91a42')
+    fetch(baseURL)
       .then((res) => res.json())
       .then((data) => {
+        data.data_batch = new Date(data.data_batch).toLocaleString('it')
         setData(data);
         setLoading(false);
       });
   }, []);
- 
+
   if (isLoading) return <p>Loading...</p>;
   if (!data) return <p>No data</p>;
- 
+
   return (
     <div>
       <h1>{data.uuid}</h1>
