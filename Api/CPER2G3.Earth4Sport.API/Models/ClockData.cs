ffcfbedc425d1a05f0cd8eb4074c7bf49eb8a6b8
Raffaele Puntin <raffaele.puntin@stud.itsaltoadriatico.it>
2023-07-05 10:08:51 +0200

    Isolato front-end sul branch

--- Api/CPER2G3.Earth4Sport.API/Models/ClockData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CPER2G3.Earth4Sport.API.Models {
-    public class ClockData {
-        public string uuid { get; set; }
-        public int n_batch { get; set; }
-        public DateTime data_batch { get; set; }
-    }
-}
--- Simulatore/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  startSwimming: () => ipcRenderer.send('startSwimming'),
-  stopSwimming: () => ipcRenderer.send('stopSwimming'),
-  closeWindow: () => ipcRenderer.send('closeWindow')
-})
