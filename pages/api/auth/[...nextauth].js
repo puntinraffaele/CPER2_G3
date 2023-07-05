0f1b261e28fe267560e994da53ec21093796ea07
Raffaele Puntin <raffaele.puntin@stud.itsaltoadriatico.it>
2023-07-05 11:15:09 +0200

    fox credential import

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { CredentialsProvider } from "next-auth/providers";
+import { CredentialsProvider } from "next-auth/providers/credentials";
 
 export const authOptions = {
   session: {
