78305722da17f50e2a78fd1a5bd0c404a0f284e8
Federico Mattiuzzi <79696658+JLarn@users.noreply.github.com>
2023-07-05 23:29:40 +0200

    Update login.js

--- components/login.js
+++ components/login.js
@@ -4,15 +4,19 @@ import { redirect } from "next/dist/server/api-utils";
 import { useEffect, useState } from 'react'
 
 export default function LoginComponent() {
-    let login = async (event) => {
-        event.preventDefault()
-        let credentials = {
-            username: event.target.usr.value,
-            password: event.target.psw.value
-        }
+
+    const [username, setUsername] = useState('');
+    const handleUsernameChange = (e) => setUsername(e.target.value);
+    const [password, setPassword] = useState('');
+    const handlePasswordChange = (e) => setPassword(e.target.value);
+
+    let login = async () => {
         const res = await fetch(baseURL + 'login', {
             method: 'POST',
-            body: JSON.stringify(credentials)
+            body: JSON.stringify({
+                username,
+                password
+            })
         }).then(res => {
             console.log(res)
             if (!res.ok) {
@@ -24,20 +28,19 @@ export default function LoginComponent() {
         })
 
         if (res) {
-
             sessionStorage.setItem('jwt_bearer', res)
         }
     }
 
     return (
         <>
-            <form onSubmit={login} method="post" className="flex flex-col items-center justify-center md:h-screen lg:py-0">
+            <div className="flex flex-col items-center justify-center md:h-screen lg:py-0">
                 <label for="usr">Utente:</label>
-                <input type="text" id="usr" />
+                <input type="text" value={username} placeholder="Giuseppe" onChange={handleUsernameChange} />
                 <label for="psw">Password:</label>
-                <input type="password" id="psw" />
-                <button type="submit">Submit</button>
-            </form>
+                <input type="password" value={password} onChange={handlePasswordChange} />
+                <button type="button" onClick={login}>Submit</button>
+            </div>
         </>
     )
 }
